fa16f6d84ae89357de0b3728256c066817a421e9
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2018-04-20 23:19:25 -0700

    Add eventide db structure

---
+++ app/schema/indexes/message_id.pgsql
@@ -0,0 +1,3 @@
+CREATE INDEX CONCURRENTLY  "messages_id_idx"
+  ON "public"."messages"
+  USING btree(id ASC NULLS LAST);
--- app/setup.sql
+++
@@ -1,3 +0,0 @@
-\set database_name `echo "$PGDATABASE"`
-DROP DATABASE IF EXISTS :"database_name";
-CREATE DATABASE :"database_name";
